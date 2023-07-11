639e716a59ef897c4b45c4e7c738060855fb30ce
tam11a <ibrahimsadiktamim@gmail.com>
2023-07-11 15:59:17 +0600

    updated: quote logic

--- src/App.tsx
+++ src/App.tsx
@@ -13,20 +13,32 @@ const App: React.FC = () => {
 					/>
 					<button className="font-bold font-serif rounded text-sm">
 						<span className="hidden sm:inline">Have New Idea?</span>{" "}
-						<span className="text-primary underline-offset-4 hover:underline ml-1">
+						<a
+							href="mailto:hello@definedsolution.com"
+							className="text-primary underline-offset-4 hover:underline ml-1"
+						>
 							GET A QUOTE
-						</span>
+						</a>
 					</button>
 				</header>
 				<main className="relative min-h-[72vh]">
 					<div className="relative">
 						<h1 className="absolute top-2/3 lg:top-3/4 -translate-y-1/2 left-1/2 -translate-x-1/2 text-[10vw] md:text-[5vw] leading-none font-koulen">
-							<span>Website</span> <br /> Under Construction.
+							<span>Website</span> <br /> Under Construction. <br />{" "}
 						</h1>
 						<Halo />
 					</div>
 				</main>
-				<footer className="relative">
+				<footer className="relative pb-4">
+					<div className="flex flex-row items-center justify-center my-7 gap-1">
+						<p>Say Hello at </p>
+						<a
+							href="mailto:hello@definedsolution.com"
+							className="text-primary relative underline-offset-4 underline !lowercase"
+						>
+							hello@definedsolution.com
+						</a>
+					</div>
 					<Marquee />
 				</footer>
 			</div>
